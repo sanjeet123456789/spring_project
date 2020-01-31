56db98cc59bcf5f22c8927847a346747b9d79c92
sanjeet123456789 <sanjeetpal123456789@gmail.com>
2020-02-01 00:01:32 +0530

    creating quick start project

---
+++ CloseApp/src/main/java/com/pal/CloseApp/App.java
@@ -0,0 +1,15 @@
+package com.pal.CloseApp;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! mavaen" );
+    }
+}
