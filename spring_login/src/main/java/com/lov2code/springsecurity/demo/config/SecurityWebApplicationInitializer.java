b74764f0ba693a7a8e923326ca55712fcb11355a
sanjeet123456789 <sanjeetpal123456789@gmail.com>
2020-01-31 22:17:35 +0530

    adding spring-login system

---
+++ spring_login/src/main/java/com/lov2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.lov2code.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
