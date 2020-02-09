4f156fd323281cce6271234266ab7c095fdfc8ab
sanjeet123456789 <sanjeetpal123456789@gmail.com>
2020-02-09 22:46:17 +0530

    spring rest

---
+++ sanjeet_pal_spring_rest_api/src/main/java/com/pal/springdemo/rest/controller/Travel.java
@@ -0,0 +1,17 @@
+package com.pal.springdemo.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Travel {
+	
+	@GetMapping("/hello")
+	public String sethello() {
+		System.out.println("hello from test");
+		return "hello";
+	}
+
+}
