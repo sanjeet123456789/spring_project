70f4beb37e2d4562c3dd1b14efa7b1a56ccf8652
sanjeet123456789 <sanjeetpal123456789@gmail.com>
2020-02-22 22:55:19 +0530

    Revert "Revert "adding subject with foreign key binding""
    This reverts commit af7c7c79a9ef2be15e8b4f67b4f2bd45ba6fc028.

--- major_project/src/main/java/com/pal/service/CustomerService.java
+++ major_project/src/main/java/com/pal/service/CustomerService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.pal.entity.Class_t;
 import com.pal.entity.Customer;
+import com.pal.entity.Subject_t;
+import com.pal.entity.Teachers_t;
 
 
 
@@ -28,6 +30,30 @@ public interface CustomerService {
 
 	public void deleteClass_t(int theId);
 	
+	
+	
+	
+	
+	
+	public List<Teachers_t> getTeachers_ts();
+
+	public void saveTeachers_t(Teachers_t theTeachers_t);
+
+	public Teachers_t getTeachers_t(int theId);
+
+	public void deleteTeachers_t(int theId);
+	
+	
+	
+	
+	
+	public List<Subject_t> getSubject_ts();
+
+	public void saveSubject_t(Subject_t theSubject_t);
+
+	public Subject_t getSubject_t(int theId);
+
+	public void deleteSubject_t(int theId);
 	
 	
 	
