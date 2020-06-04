96a622339d0f935c6d48976ea803c38f085485dd
sanjeet123456789 <sanjeetpal123456789@gmail.com>
2020-06-04 15:06:17 +0530

    creating spring-boot paytm gateway

---
+++ spring_boot/Advance/maven_pom_adv/src/main/java/pal_developer/Pal.java
@@ -0,0 +1,55 @@
+package pal_developer;
+
+public class Pal {
+	
+	/*maven build lifecycle
+	 * Validate
+	 * compile
+	 * test
+	 * package
+	 * integration test
+	 * verify
+	 * install
+	 * deploy
+	 */
+	/*
+	help: https://maven.apache.org/scm/git.html
+	help: https://help.github.com/en/packages/using-github-packages-with-your-projects-ecosystem/configuring-apache-maven-for-use-with-github-packages
+	hrlp: https://maven.apache.org/plugins/index.html
+	 * git import in pom.xml
+	 * javen compiler target /java version
+	 * configuration for failsafe-plugins /jar_plugins/surefile_plugins
+	 * properties
+	 * plugins
+	 * SCM url
+	 * AUTHOR
+	 * organization
+	 * issuemanagement
+	 * contiginous integration
+	 * 
+	 */
+	/*
+
+	help: http://tutorials.jenkov.com/maven/publish-to-central-maven-repository.html
+	configure pom file with project coordinate
+	Configure POM file with project name
+	Configure POM file with project description
+	Configure POM file with project URL
+	Configure POM file with project license
+	Configure POM file with project developers
+	Configure POM file with project distribution management
+
+	:-configure project POM plugins:	
+    	Configure POM file for Nexus staging plugin
+    	Configure POM file for Sources
+    	Configure POM file for JavaDoc
+    	Configure POM file for Signing
+ 	:-Steps for your Maven settings.xml file:
+    	Configure Sonatype account login
+    	Configure PGP key used to sign JAR files
+
+	*/
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
---
+++ spring_boot/spring-boot-paytm-app/src/main/java/com/pal/sanjeet/spring/paytm/app/SpringBootPaytmAppApplication.java
@@ -0,0 +1,13 @@
+package com.pal.sanjeet.spring.paytm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaytmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPaytmAppApplication.class, args);
+	}
+
+}
